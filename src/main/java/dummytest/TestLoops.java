dc3ffb54f7f84f3251fe1e2ea14f80de27b6e5da
rcmaddali <ravimaddali@ravis-Air.pavlov.gateway>
2017-06-04 18:45:37 -0400

    dummy push

---
+++ src/main/java/dummytest/TestLoops.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package dummytest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author ravimaddali
+ *
+ */
+public class TestLoops {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//assert.assertTrue(true);
+		//fail("Not yet implemented");
+		System.out.println("test");
+		
+	}
+
+}
